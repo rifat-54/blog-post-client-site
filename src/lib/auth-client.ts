c623bdd98bee67193f96e2fc0131ed572d22da07
rifat-54 <rifat999por@gmail.com>
2026-02-16 22:48:49 +0600

    impliment social login

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,12 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "http://localhost:5000",
+
+     socialProviders: {
+        google: { 
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string, 
+            // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 })
