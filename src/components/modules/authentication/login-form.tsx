01e74810a4e88c649670a7fdd4279d98df2ca4c0
rifat-54 <rifat999por@gmail.com>
2026-02-19 16:09:00 +0600

    implimnet google login

--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -24,10 +24,15 @@ export function LoginForm({
 
   const handleGoogleLogin=async()=>{
     const data=await authClient.signIn.social({
-      provider:"google"
+      provider:"google",
+      callbackURL:"http://localhost:3000",
     })
     console.log("data",data)
   }
+
+  const session=authClient.useSession()
+  console.log(session)
+
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,10 +3,5 @@ export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "http://localhost:5000",
 
-     socialProviders: {
-        google: { 
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string, 
-            // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-        }, 
-    },
+    
 })
