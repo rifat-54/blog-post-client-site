c580170ce3842120dcf31af24b335a5b22e0cfd2
rifat-54 <rifat999por@gmail.com>
2026-05-01 11:43:27 +0600

    impliment zoc validation

--- src/components/modules/authentication/register-form.tsx
+++ src/components/modules/authentication/register-form.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,30 +7,40 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 
+import { useForm } from "@tanstack/react-form";
 
-import {useForm} from "@tanstack/react-form"
+import * as z from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(1, "This field is required"),
+  password: z.string().min(8, "Minimum lenght is 8"),
+  email: z.email(),
+});
 
 export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
-  
-  const form=useForm({
-    defaultValues:{
-      name:"",
-      email:"",
-      password:""
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
     },
-    onSubmit:async({value})=>{
-      console.log(value)
-    }
-  })
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+    },
+  });
 
   return (
     <Card {...props}>
@@ -41,62 +51,81 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form id="register-form" onSubmit={(e)=>{
-          e.preventDefault();
-          form.handleSubmit()
-        }}>
-
-        <FieldGroup>
-          <form.Field name="name" children={(field)=>{
-            return(
-              <Field>
-                <FieldLabel htmlFor={field.name}>Name</FieldLabel>
-                <Input
-                type="text"
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onChange={(e)=>field.handleChange(e.target.value)}
-                />
-              </Field>
-            )
-          }}>  
-
-          </form.Field>
-          <form.Field name="email" children={(field)=>{
-            return(
-              <Field>
-                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
-                <Input
-                type="email"
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onChange={(e)=>field.handleChange(e.target.value)}
-                />
-              </Field>
-            )
-          }}>  
-
-          </form.Field>
-          <form.Field name="password" children={(field)=>{
-            return(
-              <Field>
-                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
-                <Input
-                type="password"
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onChange={(e)=>field.handleChange(e.target.value)}
-                />
-              </Field>
-            )
-          }}>  
-
-          </form.Field>
-
-        </FieldGroup>
+        <form
+          id="register-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <FieldGroup>
+            <form.Field
+              name="name"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                    <Input
+                      type="text"
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                     {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            ></form.Field>
+            <form.Field
+              name="email"
+              children={(field) => {
+                                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field>
+                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                    <Input
+                      type="email"
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                       {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            ></form.Field>
+            <form.Field
+              name="password"
+              children={(field) => {
+                                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input
+                      type="password"
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                       {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            ></form.Field>
+          </FieldGroup>
 
           {/* <FieldGroup>
             <Field>
@@ -144,12 +173,13 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
           </FieldGroup> */}
 
           {/* <Button type="submit">Submit</Button> */}
-
         </form>
       </CardContent>
       <CardFooter className="flex justify-end">
-        <Button form="register-form" type="submit">Submit</Button>
+        <Button form="register-form" type="submit">
+          Submit
+        </Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
